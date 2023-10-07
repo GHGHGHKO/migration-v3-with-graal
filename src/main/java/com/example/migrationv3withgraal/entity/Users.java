f535d7ef8812c11a5f3358353bc8c94a4e3baa2b
GHGHGHKO <gudrb963@gmail.com>
2023-10-07 18:37:21 +0900

    feat: add `Users` entity

---
+++ src/main/java/com/example/migrationv3withgraal/entity/Users.java
@@ -0,0 +1,32 @@
+package com.example.migrationv3withgraal.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+
+@Builder
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class Users {
+
+    @Id
+    @Column(name = "id")
+    private UUID id = UUID.randomUUID();
+
+    @Column(name = "user_name")
+    private String userName;
+
+    @Column(name = "user_nickname")
+    private String userNickname;
+
+    @Column(name = "is_quit")
+    private boolean isQuit;
+}
