2d9677daf259b54af88b72712b9fe6425fa50c11
GHGHGHKO <gudrb963@gmail.com>
2023-10-07 12:52:23 +0900

    Initial commit

---
+++ src/main/java/com/example/migrationv3withgraal/MigrationV3WithGraalApplication.java
@@ -0,0 +1,13 @@
+package com.example.migrationv3withgraal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigrationV3WithGraalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MigrationV3WithGraalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/migrationv3withgraal/MigrationV3WithGraalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.migrationv3withgraal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MigrationV3WithGraalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
