a32624f87b5d264e9fa9266918080965b2584faa
GHGHGHKO <gudrb963@gmail.com>
2023-10-08 17:12:32 +0900

    feat: add SaveUserTasklet.java

---
+++ src/main/java/com/example/migrationv3withgraal/tasklet/SaveUserTasklet.java
@@ -0,0 +1,22 @@
+package com.example.migrationv3withgraal.tasklet;
+
+import com.example.migrationv3withgraal.service.UsersService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class SaveUserTasklet implements Tasklet {
+
+    private final UsersService usersService;
+
+    @Override
+    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+        usersService.saveUser();
+        return RepeatStatus.FINISHED;
+    }
+}
