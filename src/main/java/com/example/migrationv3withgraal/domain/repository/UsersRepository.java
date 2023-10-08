a78c7de40486895de904f13de2eb1ca0947650d0
GHGHGHKO <gudrb963@gmail.com>
2023-10-08 17:02:05 +0900

    feat: add UsersRepository.java

---
+++ src/main/java/com/example/migrationv3withgraal/domain/repository/UsersRepository.java
@@ -0,0 +1,12 @@
+package com.example.migrationv3withgraal.domain.repository;
+
+import com.example.migrationv3withgraal.domain.entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UsersRepository extends JpaRepository<Users, UUID> {
+
+}
